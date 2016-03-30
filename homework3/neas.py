207ee47cce70c3f8242b7f20fa35a99fc0692f9c
evanhessler <ehessler@lion.lmu.edu>
2016-03-30 01:59:38 -0700

    completed neas

---
+++ homework3/neas.py
@@ -0,0 +1,14 @@
+class Neas(Backtracker):
+	def __init__(self, slots, values):
+		self.slots = slots
+		self.values = values
+
+	@staticmethod
+	def isValidPartialSolution(slotIndex):
+		newSlots = slots[:slotIndex]
+		for x in range(1, slotIndex):
+			slotsDivided = [newSlots[i:i+x] for i in range(0, len(newSlots), x)]
+			for a, b in zip(slotsDivided, slotsDivided[1:]):
+				if a == b:
+					return False
+		return True
