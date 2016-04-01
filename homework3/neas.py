3ac0c2606d06dd58d7180d4d974f653b0dbf5462
evanhessler <ehessler@lion.lmu.edu>
2016-03-31 21:46:07 -0700

    Changed from camel case to underscore

--- homework3/neas.py
+++ homework3/neas.py
@@ -5,11 +5,11 @@ class Neas(Backtracker):
 		self.slots = slots
 		self.values = values
 
-	def isValidPartialSolution(self, slotIndex):
-		newSlots = slots[:slotIndex]
-		for x in range(1, slotIndex):
-			slotsDivided = [newSlots[i:i+x] for i in range(0, len(newSlots), x)]
-			for a, b in zip(slotsDivided, slotsDivided[1:]):
+	def is_valid_partial_solution(self, slot_index):
+		new_slots = slots[:slot_index]
+		for x in range(1, slot_index):
+			slots_divided = [new_slots[i:i+x] for i in range(0, len(new_slots), x)]
+			for a, b in zip(slots_divided, slots_divided[1:]):
 				if a == b:
 					return False
 		return True
