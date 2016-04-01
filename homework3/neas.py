cddaeb828f5fcb33c8b401676bc57a58f4f41ea2
evanhessler <ehessler@lion.lmu.edu>
2016-03-31 17:18:02 -0700

    Now works in separate file

--- homework3/neas.py
+++ homework3/neas.py
@@ -1,10 +1,11 @@
+from backtracker import Backtracker
+
 class Neas(Backtracker):
 	def __init__(self, slots, values):
 		self.slots = slots
 		self.values = values
 
-	@staticmethod
-	def isValidPartialSolution(slotIndex):
+	def isValidPartialSolution(self, slotIndex):
 		newSlots = slots[:slotIndex]
 		for x in range(1, slotIndex):
 			slotsDivided = [newSlots[i:i+x] for i in range(0, len(newSlots), x)]
