8610fbdddb23e206ea7dd28e76f73d5b2631c054
evanhessler <ehessler@lion.lmu.edu>
2016-04-24 15:37:50 -0700

    finished function

---
+++ homework4/subset_sum.py
@@ -0,0 +1,22 @@
+from itertools import chain, combinations
+
+def powerset(iterable):
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
+def subset_sum(numbers, value):
+	current_list = []
+	current_list_index = 0
+
+	while current_list_index != value:
+		for x in range(len(numbers) + 1):
+			current_list = numbers[:x]
+
+		for y in powerset(current_list):
+			if sum(y) == value:
+				print y
+				return True
+
+		current_list_index += 1
+
+	return False
